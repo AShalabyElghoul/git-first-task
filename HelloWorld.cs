94e24c455a637725af79a7087deb7249c10b4920
AShalabyElghoul <abdelrahman shalaby@LAPTOP-4GRETQ6T>
2024-03-20 13:57:57 +0200

    hello world added

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    public class HelloJava
+    {
+        public static void PrintHelloWorld()
+        {
+            console.WriteLine("Hello World");
+        }
+      
+    }
+}
